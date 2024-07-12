3eef1a6f7599abf62406f2e65bb77c49be7d88c0
shreyap2004 <spps6161@gmail.com>
2024-07-11 21:39:16 -0400

    Moved backend code into proper methods
    backend code now in methods so frontend can call it to get recs - not able to test if it works yet

--- backend/restaurants.py
+++ backend/restaurants.py
@@ -4,59 +4,53 @@ import requests
 import json
 import geopy.distance
 
-my_api_key = "&key=AIzaSyCxduNEld5Ek1zYcr7nlrVLhJBBwlH1Fy4"
-g = geocoder.ip('me')
-currLocCoords = (g.latlng[0], g.latlng[1])
-currLocCoordsURL = str(g.latlng[0]) + "%2C" + str(g.latlng[1])
-url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + currLocCoordsURL
+api_key = "&key=AIzaSyCxduNEld5Ek1zYcr7nlrVLhJBBwlH1Fy4"
 
-keyword = input("What type of cuisine? (Hit enter for all types)").lower().replace(" ", "%20")
-keyword = "&keyword=" + keyword
-url = url + keyword
+def get_food_recommendations(location, keyword, minprice, maxprice, opennow, radius):
+    url = build_url(keyword, minprice, maxprice, opennow, radius)
+    response = requests.request("GET", url)
+    response = json.loads(response.text)
+    status = response['status']
 
-minprice = input("Minimum price? Enter $ for inexpensive (under $10), $$ for moderately expensive ($10 - $25), $$$ for expensive ($25 - $45), $$$$ for very expensive ($50 +).")
-if minprice != "":
-    maxprice = "&minprice=" + minprice
-    url = url + maxprice
+    if status == "OK":
+        results = response["results"]
+    else:
+        print(status)
+        return []
 
-maxprice = input("Maximum price? Enter $ for inexpensive (under $10), $$ for moderately expensive ($10 - $25), $$$ for expensive ($25 - $45), $$$$ for very expensive ($50 +).")
-if maxprice != "":
-    maxprice = "&maxprice=" + maxprice
-    url = url + maxprice
+    numPlaces = len(results)
+    # restAttributesList = ["name", "businessStatus", "openNow", "priceLevel", "rating", "totalUserRatings", "distance", "address"]
+    restInstVarList = build_recs(results)
 
-opennow = input("Do you want it to be open now? (Y or N) ").lower()
-if opennow == "y":
-    opennow = "&opennow=true"
-    url = url + opennow
-elif opennow == "n":
-    opennow = "&opennow=false"
-    url = url + opennow
+    return restInstVarList
 
-radius = input("What is the maximum distance you are willing to travel in miles? ")
-if radius != "":
-    radius = str(round(float(radius) * 1609.34))
-    radius = "&radius=" + radius
-    url = url + radius
-else:
-    url = url + "&radius=3200"
+def build_url(location, keyword, minprice, maxprice, opennow, radius):
+    g = geocoder.ip('me')
+    # currLocCoords = (g.latlng[0], g.latlng[1])
+    currLocCoordsURL = str(g.latlng[0]) + "%2C" + str(g.latlng[1])
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + currLocCoordsURL
+    
+    url += "&keyword=" + keyword 
 
-type = "&type=restaurant"
-url = url + type
+    if minprice != "":
+        url += "&minprice=" + minprice
 
-url = url + my_api_key
-response = requests.request("GET", url)
+    if maxprice != "":
+        url += "&maxprice=" + maxprice
 
-response = json.loads(response.text)
-status = response['status']
-if status == "OK":
-    results = response["results"]
-else:
-    print(status)
-    exit()
+    if opennow == "y":
+        url += "&opennow=true"
+    elif opennow == "n":
+        url += "&opennow=false"
 
-numPlaces = len(results)
-restAttributesList = ["name", "businessStatus", "openNow", "priceLevel", "rating", "totalUserRatings", "distance", "address"]
-restInstVarList = []
+    if radius != "":
+        url += "&radius=" + str(round(float(radius) * 1609.34))
+    else:
+        url += "&radius=3200"
+
+    url += "&type=restaurant" + api_key
+
+    return url
 
 class restaurant:
     def __init__(self):
@@ -68,74 +62,111 @@ class restaurant:
         self.totalUserRatings = -1
         self.distance = -1
         self.address = ""
-        
-for i in range(numPlaces):
-    currPlace = results[i]
-    rest = restaurant()
 
-    try:
-        name = currPlace["name"]
-        rest.name = name
-    except KeyError as e:
-        pass
+def build_recs(numPlaces, results):
+    restInstVarList = []
 
-    try:
-        businessStatus = currPlace["business_status"]
-        rest.businessStatus = businessStatus
-    except KeyError as e:
-        pass
+    for i in range(numPlaces):
+        currPlace = results[i]
+        rest = restaurant()
 
-    try:
-        openNow = currPlace["opening_hours"]["open_now"]
-        rest.openNow = openNow
-    except KeyError as e:
-        pass
+        try:
+            name = currPlace["name"]
+            rest.name = name
+        except KeyError as e:
+            pass
 
-    try:
-        priceLevel = currPlace["price_level"]
-        rest.priceLevel = priceLevel
-    except KeyError as e:
-        pass
+        try:
+            businessStatus = currPlace["business_status"]
+            rest.businessStatus = businessStatus
+        except KeyError as e:
+            pass
 
-    try:
-        rating = currPlace["rating"]
-        rest.rating = rating
-    except KeyError as e:
-        pass
+        try:
+            openNow = currPlace["opening_hours"]["open_now"]
+            rest.openNow = openNow
+        except KeyError as e:
+            pass
 
-    try:
-        totalUserRatings = currPlace["user_ratings_total"]
-        rest.totalUserRatings = totalUserRatings
-    except KeyError as e:
-        pass
+        try:
+            priceLevel = currPlace["price_level"]
+            rest.priceLevel = priceLevel
+        except KeyError as e:
+            pass
 
-    try:
-        placeCoords = currPlace["geometry"]["location"]
-        currPlaceCoords = (placeCoords["lat"], placeCoords["lng"])
-        distance = geopy.distance.geodesic(currLocCoords, currPlaceCoords).miles
-        rest.distance = distance
-    except KeyError as e:
-        pass
+        try:
+            rating = currPlace["rating"]
+            rest.rating = rating
+        except KeyError as e:
+            pass
 
-    try:
-        address = currPlace["vicinity"]
-        rest.address = address
-    except KeyError as e:
-        pass
+        try:
+            totalUserRatings = currPlace["user_ratings_total"]
+            rest.totalUserRatings = totalUserRatings
+        except KeyError as e:
+            pass
 
-    restInstVars = vars(rest)
-    restInstVarList.append(restInstVars)
+        try:
+            placeCoords = currPlace["geometry"]["location"]
+            currPlaceCoords = (placeCoords["lat"], placeCoords["lng"])
+            distance = geopy.distance.geodesic(currLocCoords, currPlaceCoords).miles
+            rest.distance = distance
+        except KeyError as e:
+            pass
 
-for r in restInstVarList:
-    print(r["name"] + ": ")
-    for i in range(1, 8):
-        a = restAttributesList[i]
-        print("\t" + a + ": " + str(r[a]))
-    print("\n")
+        try:
+            address = currPlace["vicinity"]
+            rest.address = address
+        except KeyError as e:
+            pass
 
-randomChoice = random.choice(restInstVarList)
-print(randomChoice["name"] + ": ")
-for i in range(1, 8):
-    a = restAttributesList[i]
-    print("\t" + a + ": " + str(randomChoice[a]))
+        restInstVars = vars(rest)
+        restInstVarList.append(restInstVars)
 
+    return restInstVarList
+
+
+
+
+# keyword = input("What type of cuisine? (Hit enter for all types)").lower().replace(" ", "%20")
+# keyword = "&keyword=" + keyword
+# url = url + keyword
+
+# minprice = input("Minimum price? Enter $ for inexpensive (under $10), $$ for moderately expensive ($10 - $25), $$$ for expensive ($25 - $45), $$$$ for very expensive ($50 +).")
+# if minprice != "":
+#     minprice = "&minprice=" + minprice
+#     url = url + minprice
+
+# maxprice = input("Maximum price? Enter $ for inexpensive (under $10), $$ for moderately expensive ($10 - $25), $$$ for expensive ($25 - $45), $$$$ for very expensive ($50 +).")
+# if maxprice != "":
+#     maxprice = "&maxprice=" + maxprice
+#     url = url + maxprice
+
+# opennow = input("Do you want it to be open now? (Y or N) ").lower()
+# if opennow == "y":
+#     opennow = "&opennow=true"
+#     url = url + opennow
+# elif opennow == "n":
+#     opennow = "&opennow=false"
+#     url = url + opennow
+
+# radius = input("What is the maximum distance you are willing to travel in miles? ")
+# if radius != "":
+#     radius = str(round(float(radius) * 1609.34))
+#     radius = "&radius=" + radius
+#     url = url + radius
+# else:
+#     url = url + "&radius=3200"
+
+# type = "&type=restaurant"
+# url = url + type
+
+# url = url + my_api_key
+
+
+# for r in restInstVarList:
+#     print(r["name"] + ": ")
+#     for i in range(1, 8):
+#         a = restAttributesList[i]
+#         print("\t" + a + ": " + str(r[a]))
+#     print("\n")
