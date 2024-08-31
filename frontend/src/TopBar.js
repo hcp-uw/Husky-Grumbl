ba472b1817a1d3e02aaa4eb0b41ee029634a2e05
Neha Pinni <nehapinni1@gmail.com>
2024-08-30 19:20:40 -0500

    explore, about, contact us, home page changes

--- frontend/src/TopBar.js
+++ frontend/src/TopBar.js
@@ -8,7 +8,8 @@ const TopBar = () => {
     <div className="top-bar">
       <Link to="/home-page" className="top-bar-title">Husky-Grumbl</Link>
       <Link Link to="/contact-us" className="contact-button">Contact Us</Link>
-      <Link Link to="/explore-page" className="explore-button">Explore123</Link>
+      <Link Link to="/about" className="about-button">About</Link>
+      <Link Link to="/explore-page" className="explore-button">Explore</Link>
     </div>
   );
 }
