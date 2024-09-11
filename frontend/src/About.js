1829cc4cc818615b57b1068cef0c9a1400c7ec94
shaynams <shaynasuzuki@gmail.com>
2024-09-10 16:14:05 -1000

    changed the about page

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
-import './About.css'; 
+import './About.css';
+import title from './HG-Components/title.png';
 
 const About = () => {
   return (
     <div className="about">
-        <h1 className="about-content">At Husky Grumbl, we make dining in the UW area simple and personalized. 
+      <section className="title">
+          <img src={title} alt="our mission"></img>
+      </section>
+
+      <section className="message">
+        <h1 className="about-content">
         Whether you're planning a meal or seeking new dining experiences, 
-        our platform helps you find the perfect spot. </h1>
+        HuskyGrumbl helps you find the perfect spot. </h1>
         <h1 className="about-content">Easily filter your options by distance, price, cuisine, and more, 
         so you can focus on enjoying your time and making delicious memories.</h1>
         <h1 className="about-content">Designed with UW students in mind, Husky Grumbl streamlines your meal planning, 
         making dining decisions effortless and convenient.</h1>
+      </section>
     </div>
   );
 }
